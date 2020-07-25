9fa761726680a0f041026f35b353ecaa6c12bfe3
yooabe <heyseaz@sina.com>
2020-07-25 21:39:44 +0800

    Bug Fixes

--- js/nav.js
+++ js/nav.js
@@ -208,6 +208,9 @@ define(["jquery"], function ($) {
         })
         $(".header-nav .nav-list").on("mouseleave", ".nav-item", function () {
             $(this).removeClass("nav-item-active");
+        })
+
+        $(".site-header").mouseleave(function(){
             $("#J_navMenu").css({ display: "block" }).removeClass("slide-down").addClass("slide-up");
         })
     }
@@ -232,7 +235,6 @@ define(["jquery"], function ($) {
         }).blur(function(){
             $("#J_keywordList").removeClass("show").addClass("hide");
         })
-        $("")
     }
 
 
